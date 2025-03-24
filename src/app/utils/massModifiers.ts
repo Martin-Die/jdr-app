4c20011401749add86e32f31cf3489659814c192
Martin-Die <martin.die.7559@gmail.com>
2025-03-24 10:30:14 +0100

    masses modifier stats accurate

--- src/app/utils/massModifiers.ts
+++ src/app/utils/massModifiers.ts
@@ -4,38 +4,44 @@ export interface MassModifier {
   degatCAC: number;
 }
 
-export function calculerMasse(poids: number, taille: number): number {
-  return poids + taille;
-}
-
 export function getMassModifier(poids: number, taille: number): MassModifier {
-  const masse = calculerMasse(poids, taille);
+  const masse = poids + taille;
 
   // Définition des seuils de base (jusqu'à 300)
   const seuilsBase = [
-    { min: 50, max: 100, mod: { esquive: 8, evitement: 4, degatCAC: -8 } },
-    { min: 100, max: 150, mod: { esquive: 6, evitement: 3, degatCAC: -6 } },
-    { min: 150, max: 200, mod: { esquive: 4, evitement: 2, degatCAC: -4 } },
-    { min: 200, max: 240, mod: { esquive: 2, evitement: 1, degatCAC: -2 } },
-    { min: 240, max: 260, mod: { esquive: 0, evitement: 0, degatCAC: 0 } },
-    { min: 260, max: 300, mod: { esquive: -2, evitement: -1, degatCAC: 2 } }
+    { min: 50, max: 100, stats: { esquive: 8, evitement: 4, degatCAC: -8 } },
+    { min: 100, max: 150, stats: { esquive: 6, evitement: 3, degatCAC: -6 } },
+    { min: 150, max: 200, stats: { esquive: 4, evitement: 2, degatCAC: -4 } },
+    { min: 200, max: 240, stats: { esquive: 2, evitement: 1, degatCAC: -2 } },
+    { min: 240, max: 260, stats: { esquive: 0, evitement: 0, degatCAC: 0 } },
+    { min: 260, max: 300, stats: { esquive: -2, evitement: -1, degatCAC: 2 } }
   ];
 
   // Vérifier d'abord les seuils de base
   for (const seuil of seuilsBase) {
     if (masse > seuil.min && masse <= seuil.max) {
-      return seuil.mod;
+      return seuil.stats;
     }
   }
 
   // Si la masse est supérieure à 300, calculer le modificateur progressif
   if (masse >= 300) {
-    const palier = Math.floor((masse - 300) / 100);
-    if (palier <= 7) { // Limite à 8 paliers (jusqu'à 1000)
+    let palier = 1;
+    let rangeMin = 300;
+    let rangeMax = 400;
+
+    while (masse > rangeMax) {
+      palier++;
+      rangeMin = rangeMax;
+      rangeMax += (100 * palier);
+      console.log(rangeMax)
+    }
+
+    if (masse <= rangeMax) {
       return {
-        esquive: -2 * (palier + 2),
-        evitement: -1 * (palier + 2),
-        degatCAC: 2 * (palier + 2)
+        esquive: -2 * (palier),
+        evitement: -1 * (palier),
+        degatCAC: 2 * (palier)
       };
     }
   }
